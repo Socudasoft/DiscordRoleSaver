4fa276a87482af1007ec68839d1f2aa01423534b
SowahCore <SilverCraft226@gmail.com>
2020-12-21 21:21:55 +0100

    Update pseudo

--- bot.js
+++ bot.js
@@ -19,11 +19,9 @@ function commandProcess(msg){
     let splitCommand = fullCommand.split(' ');
     splitCommand = splitCommand.filter(function(e){return e});
     let primaryCommand = splitCommand[0];
-    let arguments = splitCommand.slice(1);
-
 
     if (!msg.member.permissions.has(8)) {
-        msg.reply("tu n'as pas les perms, misérable geux");
+        msg.reply("tu n'as pas les perms, misérable gueux");
         return;
     }
 
@@ -36,37 +34,43 @@ function commandProcess(msg){
             loadRoles(msg);
             break;
         default:
-            msg.reply("this command doesn't exist.");
+            msg.reply("fdp.");
     }
 }
 
 function saveRoles(msg){
     let member = msg.mentions.members.first();
-    if (!member) { msg.reply("aucun debilus portant ce pseudo trouvé, Cheh !").catch(console.error); return; }
+    if (!member) { msg.reply("tu dois tag quelqu'un du serv débilus !").catch(console.error); return; }
     let roles = member._roles;
     fs.readFile('data.json', 'utf8', (err, data) => {
         if (err) throw err;
         if (data === "") return;
         let json = JSON.parse(data);
         if (json[member.user.id] === undefined) json[member.user.id] = {};
-        json[member.user.id][member.guild.id] = roles;
+        json[member.user.id][member.guild.id] = {
+            pseudo: member.nickname,
+            roles: roles
+        };
         fs.writeFile('data.json', JSON.stringify(json), (err) => {
             if (err) throw err;
-            msg.reply("Les rôles de ce fils de pute de <@" + member.user.id + "> ont bien été save").catch(console.error);
+            msg.reply("les rôles de ce fils de pute de <@" + member.user.id + "> ont bien été save !").catch(console.error);
         });
     });
 }
 
-function loadRoles(msg){
+async function loadRoles(msg){
     let member = msg.mentions.members.first();
     if (!member) {msg.reply("aucun membre trouvé nullos !").catch(console.error); return}
-    fs.readFile('data.json', 'utf8', (err, data) => {
+    fs.readFile('data.json', 'utf8', async (err, data) => {
         if (err) throw err;
         if (data === "") return;
         let json = JSON.parse(data);
-        let roles = json[member.user.id][member.guild.id];
-        member.roles.remove(member._roles).catch(console.error);
+        if (json[member.user.id] === undefined || json[member.user.id][member.guild.id] === undefined) {msg.reply("le membre n'a jamais été save débilus !").catch(console.error); return}
+        let pseudo = json[member.user.id][member.guild.id].pseudo;
+        let roles = json[member.user.id][member.guild.id].roles;
+        await member.roles.remove(member._roles).catch(console.error);
         member.roles.add(roles).catch(console.error);
+        member.setNickname(pseudo).catch(console.error);
 
         msg.reply("les rôles de <@" + member.user.id + "> la sale merde ont bien été chargés ! https://tenor.com/view/sardoche-sale-merde-bestof-transition-shit-gif-17260143").catch(console.error);
     });
